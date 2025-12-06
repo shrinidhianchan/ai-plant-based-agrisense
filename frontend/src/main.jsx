a9efd1c1ef131a3c0b5997603484ab0f8e7c8dec
shrinidhianchan <shrinidhianchan1@gmail.com>
2025-12-07 01:44:58 +0530

    Feature: Implemented reliable EmailJS feedback system and removed login functionality.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,14 @@
-// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// CHANGE THE IMPORT HERE
+import { HashRouter } from 'react-router-dom'; 
+import App from './App.jsx';
 
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-
-// Import your base styles
-import "./index.css"; 
-
-import "./global.css"; 
-
-const container = document.getElementById("root");
-
-if (!container) {
-  console.error('No #root element found. Check index.html for <div id="root"></div>');
-} else {
-  createRoot(container).render(
-    <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* CHANGE THE COMPONENT HERE */}
+    <HashRouter>
       <App />
-    </React.StrictMode>
-  );
-}
+    </HashRouter>
+  </React.StrictMode>,
+);
